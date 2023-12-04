7fe1b17e7a584a6db8cf1f5ac2d3e800d310998d
Joseph Paul <joepubacc@gmail.com>
2023-12-04 15:09:35 -0500

    enable webpack dev server add eslint configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,42 @@
 // config file for webpack
 
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: path.join(__dirname, 'src', 'client', 'index.jsx'),
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.js'
   },
+  mode: process.env.NODE_ENV,
   module: {
     rules: [
-			{
-				//JS and JSX
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env', '@babel/preset-react']
-					}
-				},
-			},
-			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader']
-			},
+      {
+        // JS and JSX
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-        template: './src/client/index.html'
+      template: './src/client/index.html'
     })
   ],
   resolve: {
     extensions: ['.*', '.js', '.jsx']
-  }  
-};
-
+  },
+  devServer: {
+    port: 8080
+  }
+}
