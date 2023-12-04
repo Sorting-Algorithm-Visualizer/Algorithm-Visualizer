aa2abe602649c3058045e65df1d062fab897a459
AVA <ava@pop-os.localdomain>
2023-12-04 15:41:25 -0600

    configure file

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -4,6 +4,9 @@ import React from 'react';
 import './styles.css'
 
 function App () {
+
+  const hi:string = 'Hello'
+
   return (
     <p>
       Hello World
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,9 +1,9 @@
 // this is the entry point for our front end
-import React from 'react';
+import React, { ElementType } from 'react';
 import ReactDOM from 'react-dom/client'
 import App from './app';
 
-const rootElement = document.querySelector('#root');
+const rootElement:any = document.querySelector('#root');
 const root = ReactDOM.createRoot(rootElement)
 
 
